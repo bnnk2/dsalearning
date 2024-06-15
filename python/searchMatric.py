178e030c1d36829e3c0223ab3e8f8c4d919f668c
krishnacse2010 <krishcse20@gmail.com>
2024-06-15 11:24:45 +1000

    optimized way

--- python/searchMatric.py
+++ python/searchMatric.py
@@ -21,8 +21,26 @@ def searchMatrix(matrix,elem):
     return False
 
 
+def searchMatrix_optimized(matrix, elem):
+    row_length = len(matrix)
+    col_length =  len(matrix[0])
+    i = 0
+    j = col_length-1
+
+    while i < row_length and j>=0:
+        if matrix[i][j] == elem:
+            return True
+        elif matrix[i][j] > elem:
+            j-=1
+        else:
+            i+=1
+    return False
+
+
+
 matrix = [[10,15,35,60],
           [20,45,55,65],
           [30,50,60,70],
           [40,57,76,80]]
 print(searchMatrix(matrix,50))
+print(searchMatrix_optimized(matrix,10))
